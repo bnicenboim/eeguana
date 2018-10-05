34f653d4665910c5963b9bee53d9acd0e06d9d61
bruno <bruno.nicenboim@gmail.com>
2018-10-05 11:34:49 +0200

    test dplyr

---
+++ tests/testthat/test_dplyr.R
@@ -0,0 +1,9 @@
+context("test dplyr functions")
+library(eegble)
+
+
+eegbl_av <- read_vhdr("binary-avfaces.vhdr")
+
+# just to check if something break, but this should be done with test_that
+group_by(eegbl_av,sample) %>% summarize_all(mean)
+group_by(eegbl_av,segment) %>% summarize_all(mean)
