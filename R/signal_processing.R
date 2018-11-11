ed53cb3e6b946339cad68d3f54e12d36abd103b1
bruno <bruno.nicenboim@gmail.com>
2018-11-11 20:40:18 +0100

    channel operations

--- R/signal_processing.R
+++ R/signal_processing.R
@@ -1,26 +1,3 @@
-#' Rereference channel.
-#'
-#' This function is meant to be used together with `mutate` or `mutate_all`. See the example
-#'
-#' @param x A channel.
-#' @param ... Channels that will be averaged as the reference.
-#' @return A rereferenced channel.
-#' @export
-#'
-#' @family channel
-#'
-#' @examples
-#' \dontrun{
-#' # Rereference all channels used the linked mastoids (average of the two mastoids)
-#'
-#' faces_segs %>% act_on(signal_tbl) %>%
-#'                  mutate_all(funs(rereference(., M1, M2)))
-#' }#'
-ch_rereference <- function(x, ..., na.rm = FALSE) {
-  x - vec_mean(..., na.rm = na.rm)
-}
-
-
 #' Downsample EEG data
 #'
 #' Downsample a signal_tbl by a factor `q`, using an FIR or IIR filter.
