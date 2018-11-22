961ed9ec1da9bbffafcc61b47d2f1f0a170f1715
bruno <bruno.nicenboim@gmail.com>
2018-11-22 13:34:43 +0100

    changed location of test files

--- tests/testthat/test_reading.R
+++ tests/testthat/test_reading.R
@@ -1,19 +1,21 @@
 context("Read dat files")
 library(eeguana)
 
-multiplexed_bin_bv1 <- read_vhdr("asalab_export_bv.vhdr", recording = "bv1")
 
-multiplexed_bin_bv2 <- read_vhdr("bv_export_bv_txt_bin_multi.vhdr", recording = "bv2")
-vectorized_bin_bv2 <- read_vhdr("bv_export_bv_txt_bin_vector.vhdr", recording = "bv2")
-multiplexed_ascii_bv2 <- read_vhdr(file = "bv_export_bv_txt_txt_multi.vhdr", recording = "bv2")
-vectorized_ascii_bv2 <- read_vhdr("bv_export_bv_txt_txt_vector.vhdr", recording = "bv2")
+
+multiplexed_bin_bv1 <- read_vhdr(system.file("testdata","asalab_export_bv.vhdr",package="eeguana"), recording = "bv1")
+
+multiplexed_bin_bv2 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_multi.vhdr",package="eeguana"), recording = "bv2")
+vectorized_bin_bv2 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_vector.vhdr",package="eeguana"), recording = "bv2")
+multiplexed_ascii_bv2 <- read_vhdr(system.file("testdata","bv_export_bv_txt_txt_multi.vhdr" ,package="eeguana"), recording = "bv2")
+vectorized_ascii_bv2 <- read_vhdr(system.file("testdata","bv_export_bv_txt_txt_vector.vhdr",package="eeguana"), recording = "bv2")
 
 #Integer encoding
-multiplexed_bin_bv2_int16 <- read_vhdr(file = "bv_export_bv_txt_bin_multi_16bit.vhdr", recording = "bv2")
-vectorized_bin_bv2_int16 <- read_vhdr("bv_export_bv_txt_bin_vector_16bit.vhdr", recording = "bv2")
+multiplexed_bin_bv2_int16 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_multi_16bit.vhdr" ,package="eeguana"), recording = "bv2")
+vectorized_bin_bv2_int16 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_vector_16bit.vhdr",package="eeguana"), recording = "bv2")
 
-multiplexed_bin_bv2_int32 <- read_vhdr("bv_export_bv_txt_bin_multi_32bit.vhdr", recording = "bv2")
-vectorized_bin_bv2_int32 <- read_vhdr("bv_export_bv_txt_bin_vector_32bit.vhdr", recording = "bv2")
+multiplexed_bin_bv2_int32 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_multi_32bit.vhdr",package="eeguana"), recording = "bv2")
+vectorized_bin_bv2_int32 <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_vector_32bit.vhdr",package="eeguana"), recording = "bv2")
 
 test_that("files match", {
   expect_equal(multiplexed_bin_bv2,vectorized_bin_bv2)
@@ -30,7 +32,7 @@ test_that("int files match", {
 })
 
 
-ft <- read_ft(file = "fieldtrip_matrix.mat", recording = "bv2")
+ft <- read_ft(system.file("testdata","fieldtrip_matrix.mat",package="eeguana"), recording = "bv2")
 
 
 channels_tbl(ft) <- channels_tbl(multiplexed_bin_bv2)
