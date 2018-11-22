352f41ce6f946a47648003af2f17af753f1ebb7f
bruno <bruno.nicenboim@gmail.com>
2018-11-22 14:01:37 +0100

    better doc and handling of files

--- R/signal_transformation.R
+++ R/signal_transformation.R
@@ -2,7 +2,7 @@
 #'
 #' Subtract the average or baseline of the points in a defined interval from all points in the segment.
 #'
-#' @param x An `eeg_lst` object.
+#' @param x An `eeg_lst` object or a channel.
 #' @param time A negative number indicating from when to baseline; the interval is defined as [time,0]. The default is to use all the negative times.
 #' @param sample_id A negative number indicating from when to baseline. The default is to use all the negative times. (`time` is ignored if  sample_id is used).
 #' @param ... Not in use. 
@@ -17,7 +17,7 @@
 ch_baseline <- function(x, ...) {
   UseMethod("ch_baseline")
 }
-
+#' @name ch_baseline
 #' @export
 ch_baseline.eeg_lst <- function(x, time = -Inf, sample_id = NULL, ...) {
 
@@ -33,7 +33,7 @@ ch_baseline.eeg_lst <- function(x, time = -Inf, sample_id = NULL, ...) {
               by = .id ]
   x
 }
-
+#' @name ch_baseline
 #' @export
 ch_baseline.channel_dbl <- function(x, time = -Inf, sample_id = NULL,...) {
 
