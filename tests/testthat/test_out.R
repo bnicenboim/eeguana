deb58173b27a83db013db1c3239e714a188de0ae
bruno <bruno.nicenboim@gmail.com>
2018-11-22 13:34:43 +0100

    changed location of test files

--- tests/testthat/test_out.R
+++ tests/testthat/test_out.R
@@ -2,7 +2,7 @@ context("test out functions")
 library(eeguana)
 
 
-eeg_file <- read_vhdr("bv_export_bv_txt_bin_multi.vhdr")
+eeg_file <- read_vhdr(system.file("testdata","bv_export_bv_txt_bin_multi.vhdr",package="eeguana"))
 
 # just to check if something break, but this should be done with test_that
 channel_names(eeg_file)
