d08942d2cda5787812d3743b77984408e90d8afe
bruno <bruno.nicenboim@gmail.com>
2018-11-26 09:58:20 +0100

    improved vignette of fieldtrip

--- R/plot.R
+++ R/plot.R
@@ -17,7 +17,7 @@ plot.eeg_lst <- function(x, max_sample = 64000, ...) {
   }
 
   df <- dplyr::as_tibble(x) %>% 
-        dplyr::mutate(channel = factor(channel, levels = channel))
+        dplyr::mutate(channel = factor(channel, levels = unique(channel)))
   plot <- ggplot2::ggplot(
     df,
     ggplot2::aes(x = time, y = amplitude, group = .id)
@@ -59,7 +59,7 @@ plot_gg.eeg_lst <- function(.data, x = time, y = amplitude, ..., max_sample = 64
 
   dots <- rlang::enquos(...)
   df <- dplyr::as_tibble(.data) %>% 
-        dplyr::mutate(channel = factor(channel, levels = channel))
+        dplyr::mutate(channel = factor(channel, levels = unique(channel)))
 
   plot <- ggplot2::ggplot(
     df,
