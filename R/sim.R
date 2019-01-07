17e4fa561fb07ff446c163040ba5074ddd829990
bruno <bruno.nicenboim@gmail.com>
2019-01-07 13:58:50 +0100

    irrelevant file

--- R/sim.R
+++
@@ -1,8 +0,0 @@
-# https://neuroimage.usc.edu/forums/t/questions-about-eeg-simulation/2055/3
-
-# https://github.com/lrkrol/SEREEGA
-
-# http://bbci.de/supplementary/EEGconnectivity/BBCB_paper.pdf
-
-# https://martinos.org/mne/dev/auto_examples/simulation/plot_simulate_evoked_data.html
-# https://www.martinos.org/mne/stable/auto_examples/simulation/plot_simulate_raw_data.html
