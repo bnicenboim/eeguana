6d48ed0ab7f98d9bd011cae7bded83a9ecfb9763
bnicenboim <bruno.nicenboim@gmail.com>
2021-02-14 14:51:31 +0100

    tests

--- tests/testthat/test_reading.R
+++ tests/testthat/test_reading.R
@@ -52,6 +52,7 @@ set1 <- read_set(file = system.file("testdata", "EEG01.mat", package = "eeguana"
 
 set2 <- read_set(file = system.file("testdata", "eeglab_data.set", package = "eeguana"), .recording = "eeglab")
 
+message("try set files epoched and with an external file")
 ## EDF tests
 
 edf <- read_edf(file = system.file("testdata", "asalab_export_edf_Segment_1.edf", package = "eeguana"), .recording = "edf")
