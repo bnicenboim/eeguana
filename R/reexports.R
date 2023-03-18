4fad4ebc386ec88ab3c8e858da6529aaa02aeeb6
bnicenboim <bruno.nicenboim@gmail.com>
2023-03-18 14:36:12 +0100

    reexports

---
+++ R/reexports.R
@@ -0,0 +1,30 @@
+# tidyselect ------------------------
+#' @export
+tidyselect::starts_with
+
+#' @export
+tidyselect::contains
+
+#' @export
+tidyselect::ends_with
+
+#' @export
+tidyselect::everything
+
+#' @export
+tidyselect::any_of
+
+#' @export
+tidyselect::all_of
+
+#' @export
+tidyselect::matches
+
+#' @export
+tidyselect::num_range
+
+#' @export
+tidyselect::last_col
+
+#' @export
+tidyselect::where
